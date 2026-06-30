3eed07b4ea0674f6ec1d2a9b5eff8544ea4f415b
Rish <266709254+RishOnBash@users.noreply.github.com>
2026-06-30 15:21:44 +0530

    add STDIN support

--- koi.c
+++ koi.c
@@ -1,5 +1,8 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #define GN "\033[38;5;118m" // Green
 #define GC "\033[38;5;248m" // Grey
 #define RD "\033[38;5;196m" // Red
@@ -14,26 +17,33 @@
  */
 
 int main(int argc, char *argv[]) {
-    if (argc < 2)
-        fprintf (stderr, "Usage: %s FILE ...\n", argv[0]); 
-    
-    for (int i = 1; i < argc; i++) {
-        FILE *fp = fopen(argv[i], "r");
-        if (!fp) {
-            fprintf (stderr, "> %s%s%s: %s\n", RD, argv[i], RT, strerror(errno));
+    if (argc >= 2) {
+        for (int i = 1; i < argc; i++) {
+            FILE *fp = fopen(argv[i], "r");
+            if (!fp) {
+            fprintf (stderr, "> %s%s%s: %s\n",
+                    RD, argv[i], RT, strerror(errno));
             continue;
+            }
+
+            char buffer [1024];
+            int count = 0;
+            printf ("> %s%s%s\n", GN, argv[i], RT); 
+            puts ("");
+            while (fgets(buffer, sizeof(buffer), fp)) {
+                count++;
+                printf ("%s%3d%s %s", GC, count, RT, buffer);
+            }
+            puts("");
+            fclose(fp);
         }
 
-        char buffer [1024];
-        int count = 0;
-        printf ("> %s%s%s\n", GN, argv[i], RT); 
-        puts ("");
-        while (fgets(buffer, sizeof(buffer), fp)) {
-            count++;
-            printf ("%s%3d%s %s", GC, count, RT, buffer);
-        }
-        puts("");
-        fclose(fp);
+    // read from STDIN
+    } else {
+        ssize_t b_read;    // bytes read
+        unsigned char buf [BUFSIZ];
+        while ((b_read = read(STDIN_FILENO, buf, BUFSIZ)) > 0)
+            write(STDOUT_FILENO, buf, b_read);
     }
     return 0;
 }
